54a859d8629e57e6f07636fb42eedc77f8becb67
carlos.beltran-at-674969126656 <carlos.beltran@openeb.co>
2018-03-21 08:51:20 -0500

    added '6kyu/kebabize'

---
+++ 6kyu/kebabize/index.js
@@ -0,0 +1,28 @@
+// http://www.codewars.com/kata/kebabize
+const LOWER_A = 'a'.charCodeAt(0)
+const LOWER_Z = 'z'.charCodeAt(0)
+const UPPER_A = 'A'.charCodeAt(0)
+const UPPER_Z = 'Z'.charCodeAt(0)
+
+function kebabize (str) {
+    let ans = ''
+    for (const letter of str) {
+        let n = letter.charCodeAt(0)
+        if (n >= LOWER_A && n <= LOWER_Z) {
+            ans += letter
+        }else if (n >= UPPER_A && n <= UPPER_Z) {
+            if (ans.length == 0 || n[n.length - 1] == '-') {
+                ans += letter.toLowerCase()
+            } else {
+                ans += '-' + letter.toLowerCase()
+            }
+        }
+    }
+
+    return ans
+}
+
+// console.log(kebabize('myCamelCasedString'))
+// console.log(kebabize('myCamelHas3Humps'))
+console.log(kebabize('3JkJz08mldgbxk73nmi'), 'jk-jzmldgbxknmi')
+
