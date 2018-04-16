c72292591cb37c4c2d11ab7aa706f3e05111aff8
carlos.beltran-at-674969126656 <carlos.beltran@openeb.co>
2018-04-16 14:14:26 -0500

    added 7kyu/word-values

---
+++ 7kyu/word-values/index.js
@@ -0,0 +1,16 @@
+// http://www.codewars.com/kata/word-values
+function wordValue (a) {
+    let ans = []
+    let i = 1
+    for (let word of a) {
+        word = word.replace(/\s/g, '')
+        let n = [...word].reduce((a, c) => a + (c.charCodeAt(0) - 96), 0) * i
+        ans.push(n)
+        i++
+    }
+    return ans
+}
+
+let test = ["codewars","abc","xyz"]
+
+console.log(wordValues(test))
