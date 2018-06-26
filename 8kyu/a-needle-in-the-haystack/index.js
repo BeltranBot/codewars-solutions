9817e99e4462d3d01bd951220670a1048c01e53f
carlos.beltran-at-674969126656 <canti89@gmail.com>
2018-06-26 11:18:11 -0500

    added 8kyu/a-needle-in-the-haystack

---
+++ 8kyu/a-needle-in-the-haystack/index.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/a-needle-in-the-haystack
+
+function findNeedle (haystack, needle = 'needle') {
+    return `found the needle at position ${haystack.indexOf(needle)}`
+}
+
